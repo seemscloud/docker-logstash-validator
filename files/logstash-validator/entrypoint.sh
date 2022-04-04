89e1b38ef6485c7bcf869bb95b38da6430b960e6
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2022-04-05 00:00:08 +0200

    asd

--- files/logstash-validator/entrypoint.sh
+++ files/logstash-validator/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 for PIPELINE in `ls config/pipelines` ; do
-#  logstash --config.test_and_exit -f config/pipelines/"${PIPELINE}"
-#
-#  if [ "${?}" != "0" ] ; then
-#    echo "Validation failed.. exiting.." && exit
-#  else
+  logstash --config.test_and_exit -f config/pipelines/"${PIPELINE}"
+
+  if [ "${?}" != "0" ] ; then
+    echo "Validation failed, please fix syntax.."
+  else
+    echo "Validation success, copying configuration files.."
     rm -rf /data/pipelines/"${PIPELINE}"/*
     mkdir -p /data/pipelines/"${PIPELINE}"
     cp -R config/pipelines/"${PIPELINE}"/* /data/pipelines/"${PIPELINE}"
-#  fi
+  fi
 done
